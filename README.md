f43df8b874578e2d8f19ba9279a463cd0e4f8104
marginyu <502097206@qq.com>
2017-04-11 18:02:45 +0800

    ~~

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ES6_Learning
 ES6学习
+
+http://nya.io/Node-js/promise-in-nodejs-get-rid-of-callback-hell/ 使用promise摆脱回调金字塔
+
+https://cnodejs.org/topic/56c297d671204e03637a3936  promise 与 generator的区别
+
+https://segmentfault.com/a/1190000006510526 JavaScript异步编程的终极演变
+
+ES 6: Promise , Generator
+ES 7: Async
--- src/promise.js
+++ src/promise.js
@@ -2,3 +2,18 @@
  * Created by marginyu on 17/4/11.
  */
 
+function timeout(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms, 'done');
+    });
+}
+
+let k = timeout(100);
+
+k.then((value) => {
+    console.log(value);
+});
+
+k.then((value) => {
+    console.log(`>>>${value}<<<<`);
+});
